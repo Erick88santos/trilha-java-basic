d5486a6a071d151c1acdac94d2818c366d8659c0
Erick88santos <epaas@discente.ifpe.edu.br>
2023-03-09 10:18:38 -0300

    Tipos de variáveis e Operadores

---
+++ src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+
+     public static void main(String[] args) {
+       /* String nomeCompleto = "LINGUAGEM " + "JAVA"; 
+        System.out.println(nomeCompleto);*/   
+        
+        String Concatenacao = "?";
+        
+        Concatenacao = 1+1+1+"1";
+        System.out.println(Concatenacao);
+
+        Concatenacao = 1+"1"+1;
+        System.out.println(Concatenacao);
+
+        Concatenacao = 1 + "1"+1+"1";
+        System.out.println(Concatenacao);
+
+        Concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(Concatenacao);
+
+    }
+    
+}
---
+++ src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        System.out.println("O valor do salário mínimo é  $"+salarioMinimo);
+
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+        
+    }
+
+
+}
